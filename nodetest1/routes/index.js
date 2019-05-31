0f5805c7bf007eba2d8c163178cda0a409f5e5ff
Lorna Harwood <lornapeepwreckers@gmail.com>
2019-05-31 18:01:12 +0100

    set up home page to have sign up form - no post request set up

--- nodetest1/routes/index.js
+++ nodetest1/routes/index.js
@@ -3,23 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Makers BnB' });
 });
 
-/* GET Hello World page. */
-router.get('/helloworld', function(req, res) {
-    res.render('helloworld', { title: 'Hello, World!' });
-});
 
-/* GET Userlist page. */
-router.get('/userlist', function(req, res) {
-    var db = req.db;
-    var collection = db.get('usercollection');
-    collection.find({},{},function(e,docs){
-        res.render('userlist', {
-            "userlist" : docs
-        });
-    });
-});
 
 module.exports = router;
